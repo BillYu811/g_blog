9d70be847bae340879d1599d0fec70c68b6faeec
BillYu811 <billyu811@163.com>
2020-04-15 01:52:40 +0800

    mutation add

---
+++ app/graphql/mutations/user_sign_up.rb
@@ -0,0 +1,18 @@
+module Mutations
+  class UserSignUp < BaseMutation
+    null true
+
+    argument :name, String, required: true
+    argument :password, String, required: true
+
+    field :id, ID, null: false
+    field [:errors], null: true
+    #field :msg, String, null: true
+
+    def resolve(name, password)
+      @user = User.new(name: name, password: password, password_confirmation:password)
+      return errors['sigin failed'] unless @user.save
+      user.id
+    end
+  end
+end
