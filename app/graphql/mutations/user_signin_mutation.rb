518a10831c4d5aea82e4e855ce57d4a507a03c80
BillYu811 <billyu811@163.com>
2020-04-14 22:09:28 +0800

    recommit

--- app/graphql/mutations/user_signin_mutation.rb
+++ app/graphql/mutations/user_signin_mutation.rb
@@ -1,23 +1,15 @@
 class Mutations::UserSignin < Mutations::BaseMutation
-    null true
+  null true
 
-    argument :name, String, required: true
-    argument :password, String, required: true
+  argument :name, String, required: true
+  argument :password, String, required: true
 
-    field :id, ID, null:false
-    field :error, [String], null:false
+  field :id, ID, null:false
+  field :error, [String], null:false
 
-    def resolve(name:,password:)
-        user = User.new
-        User.name = name
-        User.password = password
-        if user.save {
-            id: user.id
-            errors: []
-        }else{
-            id: nil
-            errors: ['signin failed.']
-        }
-
-    end
+  def resolve(name, password)
+    user = User.new(name: name, password: password)
+    return erros: ['sigin failed'] unless user.save
+    user.id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_many :blogs
+    has_secure_password
+
+    validates :name, presence: true
+
 end
